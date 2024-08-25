682eeea27ce574ec2fd94d76cbb7cddbd585a0f0
Dimitry Minchukov <112732061+ChiJhin@users.noreply.github.com>
2024-08-25 22:52:47 +0300

    add real api

---
+++ src/app/(admin)/dashboard/categories/default.tsx
@@ -0,0 +1,5 @@
+import Header from '@/app/components/header';
+
+export default async function DefaultPage() {
+  return <Header>Dashboard / Default Categories</Header>;
+}
---
+++ src/app/(admin)/dashboard/promotions/default.tsx
@@ -0,0 +1,5 @@
+import Header from '@/app/components/header';
+
+export default async function DefaultPage() {
+  return <Header>Dashboard / Default Promotions</Header>;
+}
